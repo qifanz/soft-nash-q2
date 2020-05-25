5f94e6130d5e5fecd6bd3ad2fde66657f3b26c34
qifanz <flavienzqf@gmail.com>
2020-05-25 16:04:11 -0400

    update nashq run script

--- run_nashq.py
+++ run_nashq.py
@@ -10,20 +10,24 @@ ground_truth_values_file = 'data/LowDimensionPEG/nash_policies.pkl'
 policies_file = 'data/' + env.get_name() + '/nashq_policies.pkl'
 evaluator = PolicyEvaluator(env, ground_truth_policies_file, ground_truth_values_file)
 init_lr = 0.1
-for i in range(5):
+lr_anneal_factor = 0.9
+total_runs = 5
+for i in range(total_runs):
     log_file = 'data/' + env.get_name() + '/nashq/log' + str(init_lr) + '.csv'
     reward_file = 'data/' + env.get_name() + '/nashq/reward' + str(init_lr) + '.pkl'
+    info = 'lr:' + str(init_lr) + ' |lr_anneal_factor:' + str(lr_anneal_factor) + 'No.' + str(i) + '/' + str(total_runs)
     policies, cumulative_reward = train(game,
                                         evaluator,
                                         log_file,
                                         policies_file,
                                         env.is_terminal_state,  # just to make life easier
                                         lr=init_lr,
-                                        lr_anneal_factor=0.9,
+                                        lr_anneal_factor=lr_anneal_factor,
                                         verbose=True,
                                         update_frequency=10000,
                                         total_n_episodes=200001,
-                                        evaluate_frequency=2000
+                                        evaluate_frequency=2000,
+                                        run_info=info
                                         )
     f = open(reward_file, 'wb')
     pickle.dump(cumulative_reward, f)
